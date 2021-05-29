97bc5f57bf40c014a41754efb966172968b5d1be
igor394 <igorsvidunovic@gmail.com>
2021-05-29 19:02:45 +0300

    create app without all any  search

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import {useCallback, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 import './App.css';
 import dataBase from '../src/data.json'
 
 function App() {
+    const [isAll, setIsAll] = useState()
     const [isRating, setIsRating] = useState(false)
     const [isGenre, setIsGenre] = useState(false)
-    const [searchArray, setSearchArray] = useState([])
-    const [rating, setRating] = useState(0)
-    const [genre, setGenre] = useState('')
+    const [array, setArray] = useState([])
+    const [name, setName] = useState('')
+    const [rating, setRating] = useState([])
+    const [genre, setGenre] = useState([])
+
+
+    useEffect(() => {
+        if (isAll) {
+            setArray(dataBase);
+            return true
+        }
+        if (name.length !== 0 && rating.length !== 0 && genre.length === 0) {
+            setArray((dataBase.filter(item => item.title.toLowerCase().includes(name))).filter(obj => rating.includes(Math.floor(obj.rating))));
+            return true;
+        }
+        if (name.length !== 0 && genre.length !== 0 && rating.length === 0) {
+            setArray((dataBase.filter(item => item.title.toLowerCase().includes(name))).filter(obj => genre.includes(obj.category)));
+            return true;
+        }
+        if (name.length !== 0 && rating.length === 0 && genre.length === 0) {
+            setArray(dataBase.filter(item => item.title.toLowerCase().includes(name)))
+            return true
+        }
+        if (name.length === 0 && rating.length !== 0 && genre.length === 0) {
+            setArray(dataBase.filter(obj => rating.includes(Math.floor(obj.rating))));
+            return true
+        }
+        if (name.length === 0 && rating.length === 0 && genre.length !== 0) {
+            setArray(dataBase.filter(obj => genre.includes(obj.category)));
+            return true
+        }
+        if (name.length === 0 && rating.length === 0 && genre.length === 0) {
+            setArray([]);
+        }
+    }, [name, rating, genre])
     const searchHandler = useCallback((event) => {
         let params = event.target.value.toLowerCase();
         if (params === '') {
-            setSearchArray([]);
+            setName('');
             return false;
         }
-        setSearchArray(dataBase.filter(item => item.title.toLowerCase().includes(params)))
+        setName(params)
     }, [])
-    const ratingOutput = (params) => {
+    const ratingOutput = useCallback((params) => {
         let filled = Array(Math.floor(params)).fill('<img src="/images/star-filled.svg" alt="arrow"/>').join('')
         let empty = Array(10 - Math.ceil(params)).fill('<img src="/images/star.svg" alt="arrow"/>').join('')
         let half = Array(1).fill('<img src="/images/half-star.svg" alt="arrow" class="half-star"/>').join('')
@@ -26,25 +59,61 @@ function App() {
             default:
                 return filled + half + empty
         }
-    }
+    }, [])
     const genreHandler = () => {
+        setIsRating(false)
+        setRating([])
         setIsGenre(previous => !previous)
-
+        if (isGenre) setGenre([])
     }
     const ratingHandler = () => {
+        setIsGenre(false)
+        setGenre([])
         setIsRating(previous => !previous)
+        if (isRating) setRating([])
     }
-    const checkboxHandler = (event) =>{
-const array = document.querySelectorAll('.checkbox')
+    const checkboxRatingHandler = (event) => {
+        let checked = event.target.checked;
+        let id = event.currentTarget.id
+        const filter = rating.filter(item => item !== +id)
+        switch (checked) {
+            case true:
+                setRating([...filter, +id])
+                break;
+            case false:
+                setRating(filter)
+                break;
+            default:
+                return rating
+        }
     }
+    const checkboxGenreHandler = (event) => {
+        let checked = event.target.checked;
+        let id = event.currentTarget.id
+        const filter = genre.filter(item => item !== id)
+        switch (checked) {
+            case true:
+                setGenre([...filter, id])
+                break;
+            case false:
+                setGenre(filter)
+                break;
+            default:
+                return genre
+        }
+    }
+    const handlerAny = () => {
+        setIsAll(previous => !previous)
+    }
+
     return (
         <div className="App">
             <div className='search-collum'>
                 <div className='search'>
                     <input type='text' className='input' placeholder='Enter movie name' onChange={searchHandler}/>
                 </div>
-                {searchArray.length > 0 && <div className='search-result'>
-                    {searchArray.map((item, index) =>
+                {array.length > 0 && <div className='search-result'>
+                    {array.map((item, index) =>
                         <div key={index - item.rating} className='item-result'>
                             <div className='result-text'>
                                 <span>{item.title}</span>
@@ -55,7 +124,6 @@ const array = document.querySelectorAll('.checkbox')
                         </div>)}
                 </div>}
             </div>
-
             <div className='rating-collum'>
                 <div className='selected' onClick={ratingHandler}>
                     <span>Rating</span>
@@ -63,55 +131,72 @@ const array = document.querySelectorAll('.checkbox')
                         <img src="/images/chevron-down.svg" alt="arrow"/>}
                 </div>
                 {isRating && <div className='rating'>
-                    <div onClick={checkboxHandler}>
-                        <input className='checkbox' name='rating' id='0' type="radio"  multiple/>
-                        <div>
-                            Any rating
-                        </div>
+                    <div onClick={handlerAny}>
+                        <label>
+                            <input className='checkbox' type="checkbox"/>
+                            <span>Any rating</span>
+                        </label>
                     </div>
-
-                    <div onClick={checkboxHandler}>
-                        <input className='checkbox' name='rating' id='1' type="radio"/>
-                        <div dangerouslySetInnerHTML={{__html: ratingOutput(1)}}></div>
+                    <div onClick={checkboxRatingHandler} id='1'>
+                        <label>
+                            <input className='checkbox' type="checkbox"/>
+                            <span dangerouslySetInnerHTML={{__html: ratingOutput(1)}}></span>
+                        </label>
                     </div>
-                    <div onClick={checkboxHandler}>
-                        <input className='checkbox' name='rating' id='2' type="radio"/>
-                        <div dangerouslySetInnerHTML={{__html: ratingOutput(2)}}></div>
+                    <div onClick={checkboxRatingHandler} id='2'>
+                        <label>
+                            <input className='checkbox' type="checkbox"/>
+                            <span dangerouslySetInnerHTML={{__html: ratingOutput(2)}}></span>
+                        </label>
                     </div>
-                    <div onClick={checkboxHandler}>
-                        <input className='checkbox' name='rating' id='3' type="radio"/>
-                        <div dangerouslySetInnerHTML={{__html: ratingOutput(3)}}></div>
+                    <div onClick={checkboxRatingHandler} id='3'>
+                        <label>
+                            <input className='checkbox' type="checkbox"/>
+                            <span dangerouslySetInnerHTML={{__html: ratingOutput(3)}}></span>
+                        </label>
                     </div>
-                    <div onClick={checkboxHandler}>
-                        <input className='checkbox' name='rating' id='4' type="radio"/>
-                        <div dangerouslySetInnerHTML={{__html: ratingOutput(4)}}></div>
+                    <div onClick={checkboxRatingHandler} id='4'>
+                        <label>
+                            <input className='checkbox' type="checkbox"/>
+                            <span dangerouslySetInnerHTML={{__html: ratingOutput(4)}}></span>
+                        </label>
                     </div>
-                    <div onClick={checkboxHandler}>
-                        <input className='checkbox' name='rating' id='5' type="radio"/>
-                        <div dangerouslySetInnerHTML={{__html: ratingOutput(5)}}></div>
+                    <div onClick={checkboxRatingHandler} id='5'>
+                        <label>
+                            <input className='checkbox' type="checkbox"/>
+                            <span dangerouslySetInnerHTML={{__html: ratingOutput(5)}}></span>
+                        </label>
                     </div>
-                    <div onClick={checkboxHandler}>
-                        <input className='checkbox' name='rating' id='6' type="radio"/>
-                        <div dangerouslySetInnerHTML={{__html: ratingOutput(6)}}></div>
+                    <div onClick={checkboxRatingHandler} id='6'>
+                        <label>
+                            <input className='checkbox' type="checkbox"/>
+                            <span dangerouslySetInnerHTML={{__html: ratingOutput(6)}}></span>
+                        </label>
                     </div>
-                    <div onClick={checkboxHandler}>
-                        <input className='checkbox' name='rating' id='7' type="radio"/>
-                        <div dangerouslySetInnerHTML={{__html: ratingOutput(7)}}></div>
+                    <div onClick={checkboxRatingHandler} id='7'>
+                        <label>
+                            <input className='checkbox' type="checkbox"/>
+                            <span dangerouslySetInnerHTML={{__html: ratingOutput(7)}}></span>
+                        </label>
                     </div>
-                    <div onClick={checkboxHandler}>
-                        <input className='checkbox' name='rating' id='8' type="radio"/>
-                        <div dangerouslySetInnerHTML={{__html: ratingOutput(8)}}></div>
+                    <div onClick={checkboxRatingHandler} id='8'>
+                        <label>
+                            <input className='checkbox' type="checkbox"/>
+                            <span dangerouslySetInnerHTML={{__html: ratingOutput(8)}}></span>
+                        </label>
                     </div>
-                    <div onClick={checkboxHandler}>
-                        <input className='checkbox' name='rating' id='9' type="radio"/>
-                        <div dangerouslySetInnerHTML={{__html: ratingOutput(9)}}></div>
+                    <div onClick={checkboxRatingHandler} id='9'>
+                        <label>
+                            <input className='checkbox' type="checkbox"/>
+                            <span dangerouslySetInnerHTML={{__html: ratingOutput(9)}}></span>
+                        </label>
                     </div>
-                    <div onClick={checkboxHandler}>
-                        <input className='checkbox' name='rating' id='10' type="radio"/>
-                        <div dangerouslySetInnerHTML={{__html: ratingOutput(10)}}></div>
+                    <div onClick={checkboxRatingHandler} id='10'>
+                        <label>
+                            <input className='checkbox' type="checkbox"/>
+                            <span dangerouslySetInnerHTML={{__html: ratingOutput(10)}}></span>
+                        </label>
                     </div>
-
-
                 </div>}
             </div>
 
@@ -122,6 +207,36 @@ const array = document.querySelectorAll('.checkbox')
                         <img src="/images/chevron-down.svg" alt="arrow"/>}
                 </div>
                 {isGenre && <div className='genre'>
+                    <div onClick={handlerAny}>
+                        <label>
+                            <input className='checkbox' type="checkbox"/>
+                            <span>Any genre</span>
+                        </label>
+                    </div>
+                    <div onClick={checkboxGenreHandler} id='Action'>
+                        <label>
+                            <input className='checkbox' type="checkbox"/>
+                            <span>Action</span>
+                        </label>
+                    </div>
+                    <div onClick={checkboxGenreHandler} id='Comedy'>
+                        <label>
+                            <input className='checkbox' type="checkbox"/>
+                            <span>Comedy</span>
+                        </label>
+                    </div>
+                    <div onClick={checkboxGenreHandler} id='Drama'>
+                        <label>
+                            <input className='checkbox' type="checkbox"/>
+                            <span>Drama</span>
+                        </label>
+                    </div>
+                    <div onClick={checkboxGenreHandler} id='Thriller'>
+                        <label>
+                            <input className='checkbox' type="checkbox"/>
+                            <span>Thriller</span>
+                        </label>
+                    </div>
 
                 </div>}
             </div>
